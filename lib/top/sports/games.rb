a6fee3fa312f1717f6d860da92808bc979d30fb6
john_glennan <glennan20@gmail.com>
2020-09-13 12:53:44 -0400

    initialized code with gem files

---
+++ lib/top/sports/games.rb
@@ -0,0 +1,10 @@
+require "top/sports/games/version"
+
+module Top
+  module Sports
+    module Games
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/top/sports/games/version.rb
@@ -0,0 +1,7 @@
+module Top
+  module Sports
+    module Games
+      VERSION = "0.1.0"
+    end
+  end
+end
